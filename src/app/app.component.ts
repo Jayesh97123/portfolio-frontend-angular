ce8128dd8162165c6c8b7a377cc3411abc83fddb
kolianita22 <kolianita784@gmail.com>
2026-03-03 20:47:14 +0530

    added about me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    AboutMeComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactMeComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-portfolio';
