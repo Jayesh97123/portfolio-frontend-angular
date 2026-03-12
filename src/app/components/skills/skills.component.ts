f04ae968dfc5aba0097d3bcb0f8222d8ee4f860d
kolianita22 <kolianita784@gmail.com>
2026-03-12 18:45:28 +0530

    done skills

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -4,8 +4,36 @@ import { Component } from '@angular/core';
   selector: 'app-skills',
   imports: [],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrl: './skills.component.scss',
 })
 export class SkillsComponent {
-
+  skills: {
+    name: string;
+    iconClass: string;
+  }[] = [
+    {
+      name: 'Git',
+      iconClass: 'bi bi-git',
+    },
+    {
+      name: 'JavaScript',
+      iconClass: 'bi bi-javascript',
+    },
+    {
+      name: 'Typescript',
+      iconClass: 'bi bi-typescript',
+    },
+    {
+      name: 'Angular',
+      iconClass: 'fa-brands fa-angular',
+    },
+    {
+      name: 'SCSS',
+      iconClass: 'fa-brands fa-sass',
+    },
+    {
+      name: 'CSS',
+      iconClass: 'fa-brands fa-css',
+    },
+  ];
 }
