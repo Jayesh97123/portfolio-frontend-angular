b6fc2f7b44cbf83e4d2520ed9c8064400276fa0f
kolianita22 <kolianita784@gmail.com>
2026-03-15 19:13:04 +0530

    added download resume

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactMeComponent } from './components/contact-me/contact-me.component';
 import { ExperianceComponent } from './components/experiance/experiance.component';
 import { HomeComponent } from './components/home/home.component';
+import html2pdf from 'html2pdf.js';
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet,
     AboutMeComponent,
     SkillsComponent,
-    ProjectsComponent,
+    // ProjectsComponent,
     ContactMeComponent,
     ExperianceComponent,
     HomeComponent,
@@ -25,10 +24,48 @@ export class AppComponent {
   title = 'angular-portfolio';
 
   scrollToSection(sectionName: string) {
-    console.log('Scrolling to section:', sectionName);
     let element = document.getElementById(sectionName);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  downloadResume() {
+    const hideNavigationContent = document.getElementById('navigation-bar');
+
+    if (hideNavigationContent) {
+      hideNavigationContent.style.display = 'none';
+    }
+
+    const hideContactMe = document.getElementById('contact-me');
+
+    if (hideContactMe) {
+      hideContactMe.style.display = 'none';
+    }
+
+    const element = document.getElementById('download-resume-content');
+
+    if (element) {
+      const options: any = {
+        margin: 10,
+        filename: 'jayesh-saindane-resume.pdf',
+        image: { type: 'jpeg', quality: 1 },
+        html2canvas: { scale: 1 },
+        jsPDF: {
+          unit: 'px',
+          format: [1851, element.offsetHeight - 375],
+          orientation: 'portrait',
+        },
+      };
+
+      html2pdf()
+        .set(options)
+        .from(element)
+        .save()
+        .then(() => {
+          hideNavigationContent.style.display = 'block';
+          hideContactMe.style.display = 'flex';
+        });
     }
   }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,8 +8,13 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class HomeComponent {
   @Output() scrollToSection = new EventEmitter<string>();
+  @Output() downloadResume = new EventEmitter<any>();
 
   onNavClick(sectionName: string) {
     this.scrollToSection.emit(sectionName);
+  }
+
+  onDownloadResume() {
+    this.downloadResume.emit();
   }
 }
